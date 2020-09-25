cd961e5057d228e7706fd730129c5c96ea705e98
avnish986 <avnishgupta986@gmail.com>
2020-09-25 21:20:48 +0530

    [Avnish]Added:uc1 player at position 0

--- src/com/gameplay/SnakeLadder.java
+++ src/com/gameplay/SnakeLadder.java
@@ -4,5 +4,7 @@ public class SnakeLadder {
 	public static final int player_count=1;
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder game");
+		int position = 0;
+		System.out.println("Player at : "+position);
 }
 }
