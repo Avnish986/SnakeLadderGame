df6af1dff78a9b2e09233014e98c7c624270bf27
avnish986 <avnishgupta986@gmail.com>
2020-09-25 21:22:35 +0530

    [Avnish]Added:uc2 player starts rolling dice

--- src/com/gameplay/SnakeLadder.java
+++ src/com/gameplay/SnakeLadder.java
@@ -6,5 +6,7 @@ public class SnakeLadder {
 		System.out.println("Welcome to Snake and Ladder game");
 		int position = 0;
 		System.out.println("Player at : "+position);
+		int valDice=(int)(Math.floor(Math.random()*10)%6)+1;
+		System.out.println("player rolled no. "+valDice);
 }
 }
