ab3335b0f392130a45c0c0309f7fd909482dbd0e
avnish986 <avnishgupta986@gmail.com>
2020-09-25 21:25:29 +0530

    [Avnish]Added:uc3 player checks for his/her advancing options

--- src/com/gameplay/SnakeLadder.java
+++ src/com/gameplay/SnakeLadder.java
@@ -2,11 +2,27 @@ package com.gameplay;
 
 public class SnakeLadder {
 	public static final int player_count=1;
+	public static final int noPlay=0;
+	public static final int ladder=1;
+	public static final int snake=2;
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder game");
 		int position = 0;
 		System.out.println("Player at : "+position);
 		int valDice=(int)(Math.floor(Math.random()*10)%6)+1;
 		System.out.println("player rolled no. "+valDice);
+		int option=(int)(Math.floor(Math.random()*10)%3);
+		
+		if(option == noPlay)
+			System.out.println("Player stays at "+position);
+		else if(option == ladder) {
+			position+=valDice;
+			System.out.println("Player moves ahead at "+position);
+		}
+		else {
+			position-=valDice;
+			System.out.println("Player moves back at "+position);
+		}
 }
 }
