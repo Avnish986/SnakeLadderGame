541f3070027a44416d9ffee10dc88b7610b6c41c
avnish986 <avnishgupta986@gmail.com>
2020-09-25 21:17:47 +0530

    [Avnish]Created:SnakeLadderGame

---
+++ src/com/gameplay/SnakeLadder.java
@@ -0,0 +1,8 @@
+package com.gameplay;
+
+public class SnakeLadder {
+	public static final int player_count=1;
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake and Ladder game");
+}
+}
